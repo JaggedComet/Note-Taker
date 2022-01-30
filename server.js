0521806f58f7e7c10160f80d09f3a925d35a8664
Marco Flores <marcoflores1029@gmail.com>
2022-01-29 19:31:58 -0800

    Finished the rest of the code, finished the directories for notes and api/notes along with the post on the api/notes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-// const api = require("./public/assets/js/index");
+const database = require("./db/db.json");
+const uniqid = require('uniqid');
+
 
 // Makes a constant for PORT to be used later to deploy
 const PORT = process.env.PORT || 3001;
@@ -12,16 +14,14 @@ const app = express();
 // Middleware for prasing JSON and urlencoded form data from the public folder
 // Serves the static files
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
-// app.use('/api', api);
 
-
-// Gets the routes to the public doc
-// Gets the index.html route
-// GET * should return the index.html file.
-app.get("/", (req, res) =>
-res.sendFile(path.join(__dirname, "/public/index.html"))
+// Gets the data from the db.json and shows it on the page
+app.get("/api/notes", (req, res) =>
+  res.json(database)
 );
 
 // GET /notes should return the notes.html file.
@@ -31,8 +31,43 @@ app.get("/notes", (req, res) =>
 );
 
 
+// Gets the routes to the public doc
+// Gets the index.html route
+// GET * should return the index.html file.
+app.get("*", (req, res) => 
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// Posts the data on the page
+app.post("/api/notes", (req, res) => {
+  const {
+    title,
+    text,
+    id
+  } = req.body;
+  // Requires a Title and Text, then adds the unique ID to it on the Array in the db.json
+  if (title && text) {
+    let response = {
+      title,
+      text,
+      id: uniqid(),
+    };
+    // Pushes the response to the database array
+    database.push(response);
+    fs.writeFile("./db/db.json", JSON.stringify(database), (err) =>
+      err ?
+      console.error(error) :
+      console.log("note added")
+    );
+    // Sends a response with a note that says note is added to list
+    res.send("note added to the list");
+  } else {
+    res.status(400).json("wrong");
+  }
+});
+
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-
